90a1e80019e998d9f3296383faf99066e9104668
William Song <30965609+Freakwill@users.noreply.github.com>
2025-07-01 17:07:15 +0800

    Add files via upload

---
+++ chattools/deepseek.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from openai import OpenAI, OpenAIError
+from mixin import ChatMixin
+
+from utils import convert, read_yaml, menu
+
+
+class DeepseekChat(ChatMixin, OpenAI):
+
+    def __init__(self, description=None, history=[], name='Assistant', model="deepseek-chat", *args, **kwargs):
+        super().__init__(
+            api_key="sk-7796b3cda6dd40abaf512835b76043fc",
+            base_url="https://api.deepseek.com",
+            *args, **kwargs)
+        self.description = description
+        self._history = history
+        self.name = name
+        self.model = model
+        self.chat_params = {}
+
+    def _reply(self, user_input, n_loop=100):
+        """The reply method of the AI chat assistant
+        
+        Args:
+            user_input (str): the prompt inputed by the user
+            n_loop (int, optional): the number of times to get response
+        """
+
+        if user_input.startswith(':'):
+            a, v = user_input[1:].split()
+            self.chat_params[a] = convert(v)
+            print(f'System: The parameter `{a}` is set to be `{v}`.')
+            return
+
+        message = {"role": "user", "content": user_input}
+        self.history.append(message)
+
+        k = 0
+        while True:
+            try:
+                response = self.chat.completions.create(
+                        model=self.model,
+                        messages=self.history,
+                        **self.chat_params)
+                break
+            except OpenAIError as e:
+                k +=1
+                if k >= n_loop:
+                    print(f"System: An error occurred after {n_loop} attempts:")
+                    raise e
+            except Exception as e:
+                print(f"An unexpected error occurred:")
+                raise e
+
+        assistant_reply = response.choices[0].message.content
+        self.history.append({"role": "assistant", "content": assistant_reply})
+        print(f"{self.name}: {assistant_reply}")
+
+
+
+roles = read_yaml()
+role, description = menu(roles)
+print(f"System: you select {role}.")
+chat = DeepseekChat(description=description)
+chat.run()
---
+++ chattools/utils.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from openai import OpenAI, OpenAIError
+from mixin import ChatMixin
+
+
+def convert(v):
+    if v in {'False', 'True'}:
+        return bool(v)
+    elif v == 'None':
+        return None
+    elif '.' in v:
+        return float(v)
+    elif v.isdigit():
+        return int(v)
+    else:
+        raise TypeError(f'The input `{v}` should be False/True/None or a number!')
+
+
+import yaml
+from pathlib import Path
+
+ROLES_PATH = Path(__file__).resolve().parent / 'roles.yml'
+
+def read_yaml(roles_path=ROLES_PATH):
+    if isinstance(roles_path, str): roles_path = Path(roles_path)
+    if not roles_path.exists():
+        raise FileNotFoundError(f"The file {roles_path} does not exist.")
+    with open(roles_path, 'r', encoding='utf - 8') as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+    return data
+
+
+def menu(roles):
+    print('System: please select one role from the following menu:')
+    print('    -------------')
+    for role, description in roles.items():
+        print(f"{role:>16}: {description}")
+    print('    -------------')
+    r = input("User: ")
+
+    for role, description in roles.items():
+        if role.startswith(r):
+            break
+    return role, description
+
+role, description = menu(roles)
+print(f"System: you select {role}.")
+chat = DeepseekChat(description=description)
+chat.run()
