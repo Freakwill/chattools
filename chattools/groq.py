0a3ebc968e263f506b29d6547e7deb62aa133d96
William <songcwzjut@163.com>
2025-12-30 19:51:18 +0800

    reply method

---
+++ chattools/groq.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from .mixin import ChatMixin
+from groq import Groq
+
+from .utils import get_api_key
+
+api_key = get_api_key('GROQ')
+
+
+class GroqChat(ChatMixin, Groq):
+
+    def __init__(self, description=None, history=[], name='Assistant', model="openai/gpt-oss-120b", *args, **kwargs):
+        super().__init__(api_key=api_key, *args, **kwargs)
+        self.description = description
+        self.name = name
+        self.model = model
+        self.chat_params = {}
+
+        self.history = [{"role": "system", "content":self.description}] + history
+
+    def _reply(self, message, max_retries=100):
+        """The reply method of the AI chat assistant
+        
+        Args:
+            message: the prompt object inputed by the user
+            max_retries (int, optional): the number of times to get response
+        """
+
+        k = 0
+        while True:
+            try:
+                return self.responses.create(
+                        model=self.model,
+                        messages=self.history + [message],
+                        **self.chat_params)
+            except SDKError as e:
+                k +=1
+                if k >= max_retries:
+                    print(f"System: An error occurred after {max_retries} attempts:")
+                    raise e
+            except Exception as e:
+                raise f"An unexpected error occurred: {e}"
+
